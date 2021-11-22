2ebad21c4a4f761b99fae1f3901ba11181cb3eef
Otanga-Otieno <otieno.otanga@gmail.com>
2021-11-22 13:09:11 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Alien Tokens</title>
+    <link rel="stylesheet" href="./Bootstrap/css/bootstrap.min.css">
+    <script src="./Bootstrap/js/bootstrap.bundle.min.js"></script>
+</head>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-12 col-md-3">
+            <div class="card" style="text-align: center;">
+                <div>
+                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+                    <button>$0.05</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-md-3">
+            <div class="card" style="text-align: center;">
+                <div>
+                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+                    <button>$0.10</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-md-3">
+            <div class="card" style="text-align: center;">
+                <div>
+                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+                    <button>$0.25</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-md-3">
+            <div class="card" style="text-align: center;">
+                <div>
+                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+                    <button>$0.50</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
