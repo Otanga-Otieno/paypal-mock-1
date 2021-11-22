86db2a1553d58328ff3870c9e7f5ead4ecd1ef18
Otanga-Otieno <otieno.otanga@gmail.com>
2021-11-22 16:51:05 +0300

    Set image height

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Alien Tokens</title>
+    <link rel="stylesheet" href="./style.css">
     <link rel="stylesheet" href="./Bootstrap/css/bootstrap.min.css">
     <script src="./Bootstrap/js/bootstrap.bundle.min.js"></script>
 </head>
