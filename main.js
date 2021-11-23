c3d1373ab9528f3883f948281a0ffcb75171d37a
Otanga-Otieno <otieno.otanga@gmail.com>
2021-11-23 19:53:20 +0300

    Implement cart
    Show total tokens and total cost

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ function qinc(qid) {
     const count = document.getElementById("qcount" + qid);
     var countQ = count.value;
     count.value = ++countQ;
+
+    cartCount();
 }
 
 function qdec(qid) {
     var count = document.getElementById("qcount" + qid);
     var countQ = count.value;
     if(countQ > 1) {count.value = --countQ} else hideQuantityCounter(qid);
-    
+
+    cartCount();
 }
 
 function showQuantityCounter(qid) {
@@ -17,6 +20,8 @@ function showQuantityCounter(qid) {
 
     var qCount = document.getElementById("qcounter" + qid);
     qCount.removeAttribute("hidden");
+
+    cartCount();
 }
 
 function hideQuantityCounter(qid) {
@@ -25,4 +30,29 @@ function hideQuantityCounter(qid) {
 
     var priceBtn = document.getElementById("pricebutton" + qid);
     priceBtn.removeAttribute("hidden");
+}
+
+function cartCount() {
+    var items = 0;
+    var totalAmount = 0.0;
+
+    for(let i=1; i<=4; i++) {
+        var qCounter = document.getElementById("qcounter" + i);
+        var price = document.getElementById("price" + i).value;
+        
+        if(window.getComputedStyle(qCounter).display !== "none") {
+            var qcount = document.getElementById("qcount" + i);
+            var amount = Number(price) * Number(qcount.value);
+            items = items + Number(qcount.value);
+            totalAmount = totalAmount + amount;
+            //console.log(amount)
+            //console.log(window.getComputedStyle(qCounter).display);
+        }
+    }
+
+    var amountDisplay = document.getElementById("totalamt");
+    amountDisplay.innerText = totalAmount;
+
+    var itemDisplay = document.getElementById("totalitm");
+    itemDisplay.innerText = items;
 }
