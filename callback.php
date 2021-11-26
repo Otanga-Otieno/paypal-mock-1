4ec0d0174b1cddb03323c0fe5304facf5a1c7a22
Otanga-Otieno <otieno.otanga@gmail.com>
2021-11-26 10:19:41 +0300

    Name tokens, modify callback
    Name tokens by type

--- callback.php
+++ callback.php
@@ -2,6 +2,10 @@
 
 require './functions.php';
 
+$alienTokenOne = $_GET['one'];
+$alienTokenTwo = $_GET['two'];
+$alienTokenThree = $_GET['three'];
+$alienTokenFour = $_GET['four'];
 $token = $_GET['token'];
 $payer_id = $_GET['PayerID'];
 
