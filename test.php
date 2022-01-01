1684e7e826d6beff5726520d1a3a94c76c16db3d
Otanga-Otieno <otieno.otanga@gmail.com>
2022-01-01 11:45:03 +0300

    Test email to send tokens

---
+++ test.php
@@ -0,0 +1,9 @@
+<?php
+
+$email = "alvinotanga@gmail.com";
+
+$alienTokens = generateMultiToken("1", "2", "2", "1");
+
+echo nl2br("Here are your tokens, they will be sent to your PayPal email too: \n");
+unrollTokens($alienTokens);
+send_tokens($email, $alienTokens);
